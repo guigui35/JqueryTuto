b5b395d0ea14d536dc0af3b2c5f37136a8186fa0
gnardizzi <guillaume.nardizzi@tisseo.fr>
2016-06-23 15:03:13 +0200

    jquery update:

--- tutoJquery.js
+++ tutoJquery.js
@@ -1,9 +1,23 @@
 $(function() {
 	$("h1").html('Hello world. Ce texte est affiché par jQuery.');
 	$("#btn1").click(function(){
-		$("p").hide();
+		$("p").toggle(200);
 	});
+	$("#btn2").click(function(){
+		var div1=$("#div1");
+		$("#div3").fadeToggle(1000);
+	    div1.toggle({
+	        opacity: '0.5',
+	        fontSize: '100px'
+	    });
+
+	});
+	$("#flip").click(function(){
+    	$("#panel").slideToggle();
+	});	
+
 });
+
 
 
 
