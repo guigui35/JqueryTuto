dcaf06a1e4eb8aeb3d8465f5dbb213fd379e3849
gnardizzi <guillaume.nardizzi@tisseo.fr>
2016-06-24 11:39:40 +0200

    ajout events:wq

--- exemplesJquery.js
+++ exemplesJquery.js
@@ -1,7 +1,3 @@
-
-
-
-
 $(sélecteur).action();
 
 $("p").hide(); // cache tous les éléments <p> du docuement html associé
@@ -9,3 +5,18 @@ $("p").hide(); // cache tous les éléments <p> du docuement html associé
 $(".test").hide(); // cache tous les éléments dont la classe est "test"
 
 $("#test").hide(); // cache l'élément dont l'id est "test"
+
+
+$("*") // Sélectionne tous les éléments
+$(this) // Sélectionne l'élément html courant
+$("p.intro") // Sélectionne tous les éléments <p> dont la classe est "intro"
+$("p:first") // Sélectionne le premier élément <p>
+$("ul li:first") // Sélectionne le premier élément <li> du premier <ul>
+$("ul li:fist-child") // Sélectionne le premier élément <li> de tous les <ul>
+$("[href]") // Sélectionne tous les éléments avec un attribut href
+$("a[target='blank']") // Sélectionne tous les éléments <a> dont la valeur de
+					   // l'attribut target est égale "_blank"
+$("a[target!='blank']") // Sélectionne tous les éléments <a> dont la valeur de
+						// l'attribut target n'est PAS égale à "_blank"
+$(":button") // Sélectionne tous les éléments <button> et les éléments <input> de type = "button"
+$("tr:even") // Sélectionne tous les éléments <tr> pairs (odd pour impairs)
--- tutoJquery.js
+++ tutoJquery.js
@@ -1,20 +1,49 @@
 $(function() {
 	$("h1").html('Hello world. Ce texte est affiché par jQuery.');
+	
 	$("#btn1").click(function(){
 		$("p").toggle(200);
 	});
+	
 	$("#btn2").click(function(){
 		var div1=$("#div1");
-		$("#div3").fadeToggle(1000);
 	    div1.toggle({
 	        opacity: '0.5',
 	        fontSize: '100px'
 	    });
-
 	});
+
+	$("#btn3").click(function(){
+		$("#div3").fadeToggle(1000);
+	});
+
 	$("#flip").click(function(){
     	$("#panel").slideToggle();
-	});	
+	});
+
+	$("p:first").fadeOut(2000).fadeIn(2000).fadeOut(2000).fadeIn(2000).fadeOut(2000).fadeIn(2000).fadeOut(2000).fadeIn(2000);
+
+	$("input").focus(function(){
+	    $(this).css("background-color", "#4caf50");
+	});
+
+    $("input").blur(function(){
+        $(this).css("background-color", "#ffffff");
+    });
+
+	$( "h2" ).hide();
+	$( ".testselect" ).select(function() {
+		$( "h2" ).fadeIn(1000);
+	});
+
+	$( "select" ).change(function () {
+    	var str = "";
+    	$( "select option:selected" ).each(function() {
+      		str += $( this ).text() + " ";
+    	});
+    	$( "#couleur" ).text( str );
+	}).change();
+
 
 });
 
